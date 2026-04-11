1233a503cfd71a7937efe4475b970c09fdb9dd63
Bri 3 <119721912+bridgetismee@users.noreply.github.com>
2026-04-10 22:45:11 -0400

    lab 7

--- app/models/user.py
+++ app/models/user.py
@@ -1,13 +1,20 @@
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
 from typing import Optional
 from pydantic import EmailStr
 
-
-class UserBase(SQLModel,):
+class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
     email: EmailStr = Field(index=True, unique=True)
     password: str
-    role:str = ""
+    role: str = ""
 
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    todos: list['Todo'] = Relationship(back_populates="user")
+
+class Todo(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    text: str
+    done: bool = False
+    user_id: int = Field(foreign_key="user.id")
+    user: User = Relationship(back_populates="todos")
